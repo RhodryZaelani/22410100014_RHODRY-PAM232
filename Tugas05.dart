dbec11b453a20f07067d21efee9a7eec5f17ffc6
RhodryZaelani <164966057+RhodryZaelani@users.noreply.github.com>
2024-04-03 21:28:00 +0700

    Add files via upload

---
+++ Tugas05.dart
@@ -0,0 +1,5 @@
+void main(){
+  var nama = 'Rhodry';
+  var alamat = 'Surabaya';
+  print('Nama : '+nama+' Alamat : '+alamat);
+}
---
+++ Tugas08.dart
@@ -0,0 +1,16 @@
+void main(){
+  int hari = 3;
+  String nama_hari="";
+  switch(hari)
+  {
+    case 1: nama_hari="Senin";break ;
+    case 2: nama_hari="Selasa";break ;
+    case 3: nama_hari="Rabu";break ;
+    case 4: nama_hari="Kamis";break ;
+    case 5: nama_hari="Jumat";break ;
+    case 6: nama_hari="Sabtu";break ;
+    case 7: nama_hari="Minggu";break ;
+    default : nama_hari="Salah";break ;
+  }
+  print(nama_hari);
+}
